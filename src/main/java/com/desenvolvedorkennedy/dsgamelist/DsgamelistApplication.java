eac44373c27f2c06a5f02ccfdb02b3a79c07afe5
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-07-12 15:40:35 -0300

    Project created

---
+++ src/main/java/com/desenvolvedorkennedy/dsgamelist/DsgamelistApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolvedorkennedy.dsgamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsgamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsgamelistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desenvolvedorkennedy/dsgamelist/DsgamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desenvolvedorkennedy.dsgamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsgamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
