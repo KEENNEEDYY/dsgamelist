66ac7ce432f15d54562a977b74ec478d8837c6aa
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-07-12 18:21:24 -0300

    Game seed

--- src/main/java/com/desenvolvedorkennedy/dsgamelist/entities/Game.java
+++ src/main/java/com/desenvolvedorkennedy/dsgamelist/entities/Game.java
@@ -16,29 +16,34 @@ public class Game {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String title;
-    
+
     @Column(name="game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    
+    @Column(columnDefinition ="TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition ="TEXT")
     private String longDescription;
 
     public Game() {
-    }    
+    }  
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
-
 
     public Long getId() {
         return this.id;
@@ -72,12 +77,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return this.platform;
+    public String getPlatforms() {
+        return this.platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return this.score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
