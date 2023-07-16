478e1d2e51676cd999af285a6fbdcdb8f71523c7
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-07-16 17:35:53 -0300

    Move games on list

--- src/main/java/com/desenvolvedorkennedy/dsgamelist/servicies/GameListService.java
+++ src/main/java/com/desenvolvedorkennedy/dsgamelist/servicies/GameListService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.desenvolvedorkennedy.dsgamelist.dto.GameListDTO;
+import com.desenvolvedorkennedy.dsgamelist.projections.GameMinProjection;
 import com.desenvolvedorkennedy.dsgamelist.repositories.GameListRepository;
+import com.desenvolvedorkennedy.dsgamelist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -15,8 +17,25 @@ public class GameListService {
     @Autowired
     GameListRepository gameListRepository;
 
+    @Autowired
+    GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         return gameListRepository.findAll().stream().map( x -> new GameListDTO(x)).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex){
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i<= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
